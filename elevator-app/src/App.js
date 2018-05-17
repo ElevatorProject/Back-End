ac70e6e05baaa28f5f4cc1f3ae6d06e274780dcb
Blair Sharpe <blair.a.sharpe@gmail.com>
2018-05-17 16:56:53 +0000

    Inserted Controller Button

--- elevator-app/src/App.js
+++ elevator-app/src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
+
+// Material UI
+import RaisedButton from 'material-ui/RaisedButton'
+import { deepOrange500 } from 'material-ui/styles/colors'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
 import logo from './elevator.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Elevator Control</h1>
-	  <h2 className="App-info">Control your elevator</h2>
-        </header>
-	<div id="ElevatorButton"></div>
-      </div>
-    );
-  }
-}
+// Theme
+const muiTheme = getMuiTheme({
+	palette: {
+		accent1Color: deepOrange500
+}})
 
-    <div id="root"></div>
-export default App;
+class App extends Component {
+	render() {
+		return (
+
+		<MuiThemeProvider muiTheme={muiTheme}>
+			<div className="App">
+			<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<h1 className="App-title">Elevator Control</h1>
+			<h2 className="App-info">Control your elevator</h2>
+			<RaisedButton type='submit' label='Controller' secondary/>
+			<div id="root"></div>
+			</header>
+			</div>
+
+		</MuiThemeProvider>
+		);
+	}
+}
+	export default App;
--- elevator-app/src/index.js
+++ elevator-app/src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ElevatorButton from './components/ElevatorButton/ElevatorButton';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<ElevatorButton />, document.getElementById('ElevatorButton'));
+/*ReactDOM.render(<ElevatorButton />, document.getElementById('root'));*/
 
 registerServiceWorker();
